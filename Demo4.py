770caf46a73b176a02904635b8f7dade06de030e
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-03 08:36:54 +0530

    This is a new files and branch added them

---
+++ Demo4.py
@@ -0,0 +1,12 @@
+class marks:
+    @staticmethod
+    def marks1(marks):
+        print(marks)
+
+    @staticmethod
+    def marks2(marks):
+        print(marks)
+
+obj = marks()
+obj.marks1(10)
+obj.marks2(10)
