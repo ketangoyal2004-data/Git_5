bc918b540a8bbd5492557712716573c748f1bab6
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-03 08:52:06 +0530

    This is a new modify maths_basic.py

--- maths_basic.py
+++ maths_basic.py
@@ -15,6 +15,16 @@ class maths:
     SI = self.profit * self.rate * self.Time / 100
     print(SI)
 
+    def square_number(self,a,b):
+        self.a = a
+        self.b = b
+        a2 = self.a * 2
+        b2 = self.b * 2
+        two_ab = 2 * self.a * self.b
+        square_a_b = a2 + two_ab + b2
+        print(square_a_b)
+
 obj = maths()
 obj.percentage(10)
 obj.simple_interset(12,23,35)
+obj.square_number(10,20)
