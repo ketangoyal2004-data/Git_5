b74c5336d1f169a8db4d307f707175bd80d31e1b
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-03 09:28:28 +0530

    This is a new files added them

---
+++ Demo3.py
@@ -0,0 +1,12 @@
+class player:
+    @staticmethod
+    def name1(name):
+        print(name)
+
+    @staticmethod
+    def name2(name):
+        print(name)
+
+obj = player()
+obj.name1('ABB')
+obj.name2('BBA')
